e4b77276a1afcdcd244b36d1db91dd70e9c4a58c
TaylorBui94 <42799810+TaylorBui94@users.noreply.github.com>
2020-12-06 02:26:18 -0800

    Update airport-DML.sql

--- airport-DML.sql
+++ airport-DML.sql
@@ -14,30 +14,30 @@ INSERT INTO airline (airlineName, hqCity, hqState,hqCountry, range) VALUES ('Che
 INSERT INTO airline (airlineName, hqCity, hqState,hqCountry, range) VALUES ('South Korean Flights International', 'Incheon', 'N/A', 'South Korea', 'INTERNATIONAL');
 
 
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('LGB', 'Fly Free');
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('LGB', 'LB Transit');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('LGB', 'Fly Free');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('LGB', 'LB Transit');
 
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('LAX', 'Fly Free');
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('LAX', 'LB Transit');
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('LAX', 'Cheap Flights Now International');
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('LAX', 'South Korean Flights International');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('LAX', 'Fly Free');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('LAX', 'LB Transit');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('LAX', 'Cheap Flights Now International');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('LAX', 'South Korean Flights International');
 
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('JWA', 'Fly Free');
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('JWA', 'LB Transit');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('JWA', 'Fly Free');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('JWA', 'LB Transit');
 
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('JFK', 'Fly Free');
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('JFK', 'LB Transit');
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('JFK', 'Cheap Flights Now International');
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('JFK', 'South Korean Flights International');
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('JFK', 'Local Queens');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('JFK', 'Fly Free');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('JFK', 'LB Transit');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('JFK', 'Cheap Flights Now International');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('JFK', 'South Korean Flights International');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('JFK', 'Local Queens');
 
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('BFI', 'Fly Free');
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('BFI', 'LB Transit');
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('BFI', 'Cheap Flights Now International');
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('BFI', 'South Korean Flights International');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('BFI', 'Fly Free');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('BFI', 'LB Transit');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('BFI', 'Cheap Flights Now International');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('BFI', 'South Korean Flights International');
 
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('ICN', 'South Korean Flights International');
-INSERT INTO airlineservice (FAAAbreviation, airlineName) VALUES ('ICN', 'Cheap Flights Now International');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('ICN', 'South Korean Flights International');
+INSERT INTO airlineservice (FAAAbbreviation, airlineName) VALUES ('ICN', 'Cheap Flights Now International');
 
 INSERT INTO plane(FAANumber, airlineName, manufacturer, model, passengerLimit, planeName) VALUES ( 1001,'Fly Free', 'Boeing', 'AB1', 25, 'AirBus1');
 INSERT INTO plane(FAANumber, airlineName, manufacturer, model, passengerLimit, planeName) VALUES ( 2001,'LB Transit', 'Boeing', 'AB5', 10, 'Airbus5');
